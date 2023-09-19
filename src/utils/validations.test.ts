a4e882d61954fc83016a24be8786f7b2eac2f7e4
Dan Doughty <ddoughty@landscapehub.com>
2023-09-19 13:58:31 -0500

    add some basic tests

---
+++ src/utils/validations.test.ts
@@ -0,0 +1,32 @@
+import { validateEmail } from './validations'
+
+describe('validateEmail function', () => {
+  it('should return true for a valid email', () => {
+    const validEmail = 'test@example.com'
+    const isValid = validateEmail(validEmail)
+    expect(isValid).toBe(true)
+  })
+
+  it('should return false for an invalid email', () => {
+    const invalidEmail = 'invalid-email.com'
+    const isValid = validateEmail(invalidEmail)
+    expect(isValid).toBe(false)
+  })
+
+  it('should handle different cases and still validate the email', () => {
+    const validEmailMixedCase = 'TesT@eXample.COM'
+    const isValid = validateEmail(validEmailMixedCase)
+    expect(isValid).toBe(true)
+  })
+
+  it('should return false for an empty string', () => {
+    const emptyEmail = ''
+    const isValid = validateEmail(emptyEmail)
+    expect(isValid).toBe(false)
+  })
+
+  it('should return false for undefined', () => {
+    const isValid = validateEmail(undefined)
+    expect(isValid).toBe(false)
+  })
+})
